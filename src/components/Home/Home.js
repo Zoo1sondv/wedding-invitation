5186ef1d4e711ecf9f6ea733ee0ece987f773ff1
Zoo1sondv <2001.sondv@gmail.com>
2025-10-07 18:56:27 +0700

    feat location

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,26 +4,24 @@ import clsx from "clsx";
 import useScrollAnimation from "../../hooks/useScrollAnimation";
 
 const Home = () => {
-  const [sectionRef, showContent] = useScrollAnimation({
-    threshold: 0.3,
-  });
-
-  const [sectionRef2, showContent2] = useScrollAnimation({
-    threshold: 0.3,
-  });
+  const [section1Ref, showSection1] = useScrollAnimation({ threshold: 0.3 });
+  const [section2Ref, showSection2] = useScrollAnimation({ threshold: 0.3 });
+  const [section3Ref, showSection3] = useScrollAnimation({ threshold: 0.3 });
+  const [section4Ref, showSection4] = useScrollAnimation({ threshold: 0.3 });
 
   return (
     <div className={style.home}>
-      <section id="home" ref={sectionRef}>
+      {/* Hero Section */}
+      <section id="home" ref={section1Ref}>
         <div className={style.section_1}>
           <img
             className={style.section_1__image}
-            src={"/assets/img/0F9A1821.JPG"}
-            alt="avatar"
+            src="/assets/img/0F9A1821.JPG"
+            alt="Wedding couple"
           />
           <div
             className={clsx(style.section_1__content, {
-              [style.show]: showContent,
+              [style.show]: showSection1,
             })}
           >
             <h1>We get married!</h1>
@@ -34,17 +32,13 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section
-        id="invitation-letter"
-        ref={sectionRef2}
-        className={clsx({
-          [style.show]: showContent2,
-        })}
-      >
+
+      {/* Invitation Section */}
+      <section id="invitation-letter" ref={section2Ref}>
         <div className={style.section_2}>
           <div
             className={clsx(style.section_2__content, {
-              [style.show]: showContent2,
+              [style.show]: showSection2,
             })}
           >
             <h1>
@@ -59,30 +53,93 @@ const Home = () => {
               We cordially invite you to attend the wedding ceremony of
             </h3>
           </div>
-          <div className={style.section_2__image}>
-            <div className={style.section_2__image__husband}>
-              <div className={style.section_2__image__husband__name}>
+
+          <div className={style.section_2__images}>
+            {/* Groom */}
+            <div
+              className={clsx(style.person, style.person__groom, {
+                [style.show]: showSection2,
+              })}
+            >
+              <div className={style.person__name}>
                 <p>Chú Rể</p>
                 ĐOÀN SƠN
               </div>
               <img
-                className={style.section_2__image__husband__image}
-                src={"/assets/img/0F9A0029.JPG"}
-                alt="husband"
+                className={style.person__image}
+                src="/assets/img/0F9A0029.JPG"
+                alt="Groom"
               />
             </div>
-            <div className={style.section_2__image__wife}>
+
+            {/* Bride */}
+            <div
+              className={clsx(style.person, style.person__bride, {
+                [style.show]: showSection2,
+              })}
+            >
               <img
-                className={style.section_2__image__wife__image}
-                src={"/assets/img/0F9A0682.JPG"}
-                alt="wife"
+                className={style.person__image}
+                src="/assets/img/0F9A0682.JPG"
+                alt="Bride"
               />
-              <div className={style.section_2__image__wife__name}>
+              <div className={style.person__name}>
                 <p>Cô Dâu</p>
                 NGUYỄN THÙY
               </div>
             </div>
           </div>
+        </div>
+      </section>
+
+      {/* Information Section */}
+      <section id="information" ref={section3Ref}>
+        <div className={style.section_3}>
+          <div
+            className={clsx(style.section_3__content, {
+              [style.show]: showSection3,
+            })}
+          >
+            Tình yêu của anh và em là một hành trình kỳ diệu, vượt qua bao thử
+            thách để cùng nhau bước đến ngày trọng đại - đám cưới của chúng
+            mình. Đám cưới này là lời cam kết chân thành, là sự bắt đầu của một
+            chương mới – nơi chúng ta cùng vun đắp tổ ấm, cùng sẻ chia mọi vui
+            buồn và cùng nắm tay nhau đi đến cuối con đường mang tên hạnh phúc.
+          </div>
+          <div
+            className={clsx(style.section_3__image, {
+              [style.show]: showSection3,
+            })}
+          >
+            <img src="/assets/img/0F9A1131.JPG" alt="Wedding couple" />
+          </div>
+        </div>
+      </section>
+
+      <section id="location" ref={section4Ref}>
+        <div className={style.section_4}>
+          <div
+            className={clsx(style.section_4__content, {
+              [style.show]: showSection4,
+            })}
+          >
+            <div className={style.section_4__content__item}>
+              <h1>NHÀ TRAI</h1>
+              <h3>Bố: Đoàn Văn Bẩy</h3>
+              <h3>Mẹ: Nguyễn Thị Ngân</h3>
+            </div>
+            <div className={style.section_4__content__item}>
+              <h1>NHÀ GÁI</h1>
+              <h3>Bố: Nuyễn Văn Thuật</h3>
+              <h3>Mẹ: Lê Thị Thanh</h3>
+            </div>
+          </div>
+
+          <img
+            src="/assets/gif/loading.gif"
+            alt="logo"
+            className={style.section_4__logo}
+          />
         </div>
       </section>
     </div>
