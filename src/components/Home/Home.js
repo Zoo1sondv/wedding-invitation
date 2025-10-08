adb5559164b8b717b01d3b9228edaebd351fae97
Zoo1sondv <2001.sondv@gmail.com>
2025-10-09 00:31:02 +0700

    add calender

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
   const [section2Ref, showSection2] = useScrollAnimation({ threshold: 0.3 });
   const [section3Ref, showSection3] = useScrollAnimation({ threshold: 0.3 });
   const [section4Ref, showSection4] = useScrollAnimation({ threshold: 0.3 });
+  const [section5Ref, showSection5] = useScrollAnimation({ threshold: 0.3 });
 
   return (
     <div className={style.home}>
@@ -124,22 +125,106 @@ const Home = () => {
             })}
           >
             <div className={style.section_4__content__item}>
-              <h1>NHÀ TRAI</h1>
-              <h3>Bố: Đoàn Văn Bẩy</h3>
-              <h3>Mẹ: Nguyễn Thị Ngân</h3>
+              <h1>Nhà Trai</h1>
+              <div className={style.section_4__content__item__info}>
+                <h3>Bố: Đoàn Văn Bẩy</h3>
+                <h3>Mẹ: Nguyễn Thị Ngân</h3>
+                <p className={style.section_4__content__item__address}>
+                  Địa Chỉ: Thôn Nghĩa La, X.Trung Chính, T.Bắc Ninh
+                </p>
+              </div>
+
+              <div className={style.section_4__content__item__button}>
+                Xem bản đồ
+              </div>
             </div>
+            <img
+              src="/assets/gif/loading.gif"
+              alt="logo"
+              className={style.section_4__logo}
+            />
             <div className={style.section_4__content__item}>
-              <h1>NHÀ GÁI</h1>
-              <h3>Bố: Nuyễn Văn Thuật</h3>
-              <h3>Mẹ: Lê Thị Thanh</h3>
+              <h1>Nhà Gái</h1>
+              <div className={style.section_4__content__item__info}>
+                <h3>Bố: Nuyễn Văn Thuật</h3>
+                <h3>Mẹ: Lê Thị Thanh</h3>
+                <p className={style.section_4__content__item__address}>
+                  Địa Chỉ: 121 Ngõ 1, Tổ 2, L.Đa Sỹ, P.Kiến Hưng, TP.Hà Nội
+                </p>
+              </div>
+              <div className={style.section_4__content__item__button}>
+                Xem bản đồ
+              </div>
             </div>
           </div>
+        </div>
+      </section>
 
-          <img
-            src="/assets/gif/loading.gif"
-            alt="logo"
-            className={style.section_4__logo}
-          />
+      <section id="events" ref={section5Ref}>
+        <div
+          className={style.section_5}
+          style={{
+            backgroundImage: `url(${process.env.PUBLIC_URL}/assets/img/anh.jpg)`,
+          }}
+        >
+          <div
+            className={clsx(style.section_5__content, {
+              [style.show]: showSection5,
+            })}
+          >
+            <h1 className={style.calendar_month}>November</h1>
+
+            <div className={style.calendar_weekdays}>
+              <div>MON</div>
+              <div>TUE</div>
+              <div>WED</div>
+              <div>THUR</div>
+              <div>FRI</div>
+              <div>SAT</div>
+              <div>SUN</div>
+            </div>
+
+            <div className={style.calendar_grid}>
+              <div className={style.empty}></div>
+              <div className={style.empty}></div>
+              <div className={style.empty}></div>
+              <div className={style.empty}></div>
+              <div className={style.empty}></div>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+              <div className={style.wedding_day}>
+                10
+                <div className={style.heart}>♥</div>
+              </div>
+              <div>11</div>
+              <div>12</div>
+              <div>13</div>
+              <div>14</div>
+              <div>15</div>
+              <div>16</div>
+              <div>17</div>
+              <div>18</div>
+              <div>19</div>
+              <div>20</div>
+              <div>21</div>
+              <div>22</div>
+              <div>23</div>
+              <div>24</div>
+              <div>25</div>
+              <div>26</div>
+              <div>27</div>
+              <div>28</div>
+              <div>29</div>
+              <div>30</div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
